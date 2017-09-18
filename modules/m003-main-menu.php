f8e0a8c8c2e6dd51aa5bf0373eeca7a7f622152e
davidguerreiro <me@davidguerreiro.com>
2017-09-18 18:39:18 +0100

    add module 2 css

--- modules/m003-main-menu.php
+++ modules/m003-main-menu.php
@@ -7,3 +7,28 @@
  */
 
  ?>
+
+ <div class="module m2">
+     <nav>
+        <ul class=m2__main-menu>
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+            <li>
+                <a href="#">Aula</a>
+            </li>
+            <li>
+                <a href="#">Actividades</a>
+            </li>
+            <li>
+                <a href="#">Fotografia</a>
+            </li>
+            <li>
+                <a href="#">Equipo</a>
+            </li>
+            <li>
+                <a href="#">Contacto</a>
+            </li>
+        </ul>
+     </nav>
+ </div>
