47ec65c144bb22a239f301a5d9fae8bb5d7b876a
davidguerreiro <me@davidguerreiro.com>
2017-09-24 16:05:43 +0100

    add module 07 css file

--- modules/m007-gallery-section.php
+++ modules/m007-gallery-section.php
@@ -7,3 +7,19 @@
  */
 
  ?>
+
+ <div class="module m7">
+    <div class="m7__section section-text">
+        <h2>Gallerias</h2>
+        <p>
+            Ya sea visitando una cueva, asistiendo a un campamento,
+            escalando o haciendo una actividad de espeleologia, en la
+            seccion de galerias encontraras todas las fotografias 
+            publicadas de forma organizada y accesible.
+        </p>
+    </div>
+    <a href="#" class="m7__section section-text">
+        <i class="fa fa-camera-retro" aria-hidden="true"></i>
+        <span>Ver Galerias</span>
+    </a>
+ </div>
