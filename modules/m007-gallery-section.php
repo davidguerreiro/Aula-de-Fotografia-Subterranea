e80a6d2d051327cfc5705032a42fa0cf354cd26f
davidguerreiro <me@davidguerreiro.com>
2017-09-24 17:07:50 +0100

    complete module 07 html and css

--- modules/m007-gallery-section.php
+++ modules/m007-gallery-section.php
@@ -9,8 +9,11 @@
  ?>
 
  <div class="module m7">
-    <div class="m7__section section-text">
-        <h2>Gallerias</h2>
+    <div class="m7__section m7__section-text">
+        <h2>
+            <i class="fa fa-picture-o" aria-hidden="true"></i>
+            Galerias
+        </h2>
         <p>
             Ya sea visitando una cueva, asistiendo a un campamento,
             escalando o haciendo una actividad de espeleologia, en la
@@ -18,8 +21,8 @@
             publicadas de forma organizada y accesible.
         </p>
     </div>
-    <a href="#" class="m7__section section-text">
+    <a href="#" class="m7__section m7__section-link">
         <i class="fa fa-camera-retro" aria-hidden="true"></i>
-        <span>Ver Galerias</span>
+        <p>Ver Galerias</p>
     </a>
  </div>
