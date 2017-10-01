e10b4e12d0e47fc30b71d75242d6f39af99d6e3d
davidguerreiro <me@davidguerreiro.com>
2017-10-01 18:48:11 +0100

    complete module 11

--- modules/m010-sidebar-facebook-group.php
+++ modules/m010-sidebar-facebook-group.php
@@ -11,17 +11,19 @@
  ?>
 
  <div class="module m10">
-    <i class="fa fa-facebook m10__main-icon" aria-hidden="true"></i>
+    <a href="https://www.facebook.com/groups/AULADEFOTOGRAFIASUBTERRANEA/?fref=ts" target="_blank" class="m10__face-icon-link">
+        <i class="fa fa-facebook m10__main-icon" aria-hidden="true"></i>
+    </a>
     <p>
         Sigue a nuestro grupo de Aula de Fotografia Subterránea en Facebook.
     </p>
     <p>
         Haz click en el icono de Facebook para visitar nuestra página en la conocida red social.
     </p>
-    <p class"m10__small-text">
+    <p class="m10__small-text">
         Este grupo es privado y para ver el contenido deberás logearte en Facebook y ser miembro del grupo.
     </p>
-    <a href="https://www.facebook.com/groups/AULADEFOTOGRAFIASUBTERRANEA/?fref=ts" target="_blank">
+    <a href="https://www.facebook.com/groups/AULADEFOTOGRAFIASUBTERRANEA/?fref=ts" target="_blank" class="m10__face-link">
         Ver grupo en Facebook
         <i class="fa fa-angle-right" aria-hidden="true"></i>
     </a>
--- modules/m011-sidebar-history.php
+++ modules/m011-sidebar-history.php
@@ -9,3 +9,84 @@
  */
 
  ?>
+
+ <div class="module m11">
+    <h3>
+        <i class="fa fa-history m11__story-icon" aria-hidden="true"></i>
+        Historial
+    </h3>
+    <ul class="m11__year-list">
+        <li>
+            <span>
+                2017
+                <i class="fa fa-angle-right m11__icon-angle" aria-hidden="true"></i>
+            </span>
+            <ul class="m11__month-list">
+                <li>
+                    <span> 
+                        - Septiembre 
+                        <i class="fa fa-angle-right m11__icon-angle" aria-hidden="true"></i>
+                    </span>
+                    <ul class="m11__post-list">
+                        <li>
+                            <a href="#">- Cueva de Ancares</a>
+                        </li>
+                        <li>
+                            <a href="#"> - Actividades con una camara</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <span> 
+                        - Octubre
+                        <i class="fa fa-angle-right m11__icon-angle" aria-hidden="true"></i>
+                    </span>
+                    <ul class="m11__post-list">
+                        <li>
+                            <a href="#">- Cueva de Ancares</a>
+                        </li>
+                        <li>
+                            <a href="#">- Actividades con una camara</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <span>
+                2016
+                <i class="fa fa-angle-right m11__icon-angle" aria-hidden="true"></i>
+            </span>
+            <ul class="m11__month-list">
+                <li>
+                    <span> 
+                        - Septiembre 
+                        <i class="fa fa-angle-right m11__icon-angle" aria-hidden="true"></i>
+                    </span>
+                    <ul class="m11__post-list">
+                        <li>
+                            <a href="#">- Cueva de Ancares</a>
+                        </li>
+                        <li>
+                            <a href="#">- Actividades con una camara</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <span>
+                         - Octubre 
+                         <i class="fa fa-angle-right m11__icon-angle" aria-hidden="true"></i>
+                    </span>
+                    <ul class="m11__post-list">
+                        <li>
+                            <a href="#"> - Cueva de Ancares</a>
+                        </li>
+                        <li>
+                            <a href="#"> - Actividades con una camara</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+ </div>
