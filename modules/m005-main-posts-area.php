8982a1fc1db50ac891840155c6d332ec044c6954
davidguerreiro <me@davidguerreiro.com>
2017-10-01 19:54:17 +0100

    working in module 12 and general thomepage template css

--- modules/m005-main-posts-area.php
+++ modules/m005-main-posts-area.php
@@ -10,7 +10,7 @@ $post_date = date( 'd-M-Y' );
 
  ?>
 
- <div class="module m4 m5">
+ <article class="module m4 m5">
     <a href="#" class="wrapper-link">
         <header class="m4__header m5__header" style="background-image : url('http://lorempixel.com/500/300');">
         </header>
@@ -44,4 +44,40 @@ $post_date = date( 'd-M-Y' );
             </ul>
         </div>
     </section>
- </div>
+ </article>
+
+ <article class="module m4 m5">
+    <a href="#" class="wrapper-link">
+        <header class="m4__header m5__header" style="background-image : url('http://lorempixel.com/500/300');">
+        </header>
+    </a>
+    <section class="m4__post-data">
+        <a href="#" class0="wrapper-link">
+            <div class="excerpt">
+                <h2 class="m5__post-title">Adelante aunque solo tengas una camara</h2>
+            </div>
+        </a>
+        <div class="post-info-wrapper">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-thumb-tack" aria-hidden="true"></i>
+                        Articulo
+                    </a>
+                </li>
+                <li>
+                    <span>
+                        <i class="fa fa-clock-o" aria-hidden="true"></i>
+                        <?php echo $post_date; ?>
+                    </span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-comment-o" aria-hidden="true"></i>
+                        4
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+ </article>
--- modules/m012-facebook-group.php
+++ modules/m012-facebook-group.php
@@ -6,4 +6,27 @@
  * @package aula/modules
  */
 
+
  ?>
+
+ <div class="module m12">
+    <div class="m12__section m12__section-text">
+        <h2>Visitanos en Facebook</h2>
+        <p>
+            Únete a nuestro grupo de Facebook para estar al tanto
+            de las ultimas novedades en Aula de Fotografia Subterranea.
+        </p>
+        <a href="https://www.facebook.com/groups/AULADEFOTOGRAFIASUBTERRANEA/?fref=ts" class="m12__external-link" target="_blank">
+            Únete
+        </a>
+        <p class="m12__small-text">
+            El grupo de Facebook es privado y deberas ser aceptado por
+            los administradores antes de unirte.
+        </p>
+    </div>
+    <div class="m12__section m12__section-link">
+        <a href="https://www.facebook.com/groups/AULADEFOTOGRAFIASUBTERRANEA/?fref=ts" class="m12__face-link" target="_blank">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+    </div>
+ </div>
