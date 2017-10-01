584e12714cf2673c2b4ada6485821062e5921b97
davidguerreiro <me@davidguerreiro.com>
2017-10-01 17:12:05 +0100

    complete module 10 and fix width th error in module 6

--- modules/m010-sidebar-facebook-group.php
+++ modules/m010-sidebar-facebook-group.php
@@ -9,3 +9,20 @@
  */
 
  ?>
+
+ <div class="module m10">
+    <i class="fa fa-facebook m10__main-icon" aria-hidden="true"></i>
+    <p>
+        Sigue a nuestro grupo de Aula de Fotografia Subterránea en Facebook.
+    </p>
+    <p>
+        Haz click en el icono de Facebook para visitar nuestra página en la conocida red social.
+    </p>
+    <p class"m10__small-text">
+        Este grupo es privado y para ver el contenido deberás logearte en Facebook y ser miembro del grupo.
+    </p>
+    <a href="https://www.facebook.com/groups/AULADEFOTOGRAFIASUBTERRANEA/?fref=ts" target="_blank">
+        Ver grupo en Facebook
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+    </a>
+ </div>
