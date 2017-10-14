78123347f9926cf7f6ade9235dee8ba21467013b
davidguerreiro <me@davidguerreiro.com>
2017-10-14 12:33:11 +0100

    add module 13 html code

--- modules/m013-newsletter-section.php
+++ modules/m013-newsletter-section.php
@@ -7,3 +7,22 @@
  */
 
  ?>
+
+ <div class="module .m13">
+    <div class="m13__section m13__section--text">
+        <h3>No te quires perder nada ?</h3>
+        <p>
+            Suscribete a nuestro servicio de email automatico.
+            Recibiras una notification en tu email cada vez que
+            una nueva entrada o una nueva galeria es publicada en
+            Aula de Fotografia Subterranea.
+        </p>
+    </div>
+    <div class="m13__section m13__section--form">
+        <form action="" class="newsletter">
+            <h3>Newsletter</h3>
+            <input type="text" name="email" class="newsletter__text-input" placeholder="Email">
+            <input type="sumit" value="Suscribirse" class="newsletter__submit-input">
+        </form>
+    </div>
+ </div>
