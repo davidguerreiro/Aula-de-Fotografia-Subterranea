0b5757dc3028abab06cdc847049b2b905efec9dc
davidguerreiro <me@davidguerreiro.com>
2017-12-23 17:41:30 +0100

    complete module 14 sidebarar search form

--- modules/m014-sidebar-search-form.php
+++ modules/m014-sidebar-search-form.php
@@ -12,11 +12,14 @@
 
  <div class="module m14">
      <h2>Busqueda</h2>
-     <form action="" method="get">
+     <form action="" method="get" class="m14__form">
          <input type="hidden" name="nonce" value="<?php echo $nonce; ?>">
          <div class="search-wrapper">
              <i class="" aria-hidden="true"></i>
              <input type="search" name="s" required>
+             <button type="submit" class="btn-sidebar-submit">
+                 <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+             </button>
          </div>
      </form>
      <p>
