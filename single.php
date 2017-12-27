843dd5f8cbca2f33a98c800d8607ae3a0b14f389
davidguerreiro <me@davidguerreiro.com>
2017-12-27 17:24:17 +0100

    add single post layout on main.

--- single.php
+++ single.php
@@ -8,24 +8,27 @@
 
 get_header();
 
+$post_date = date( 'd-m-Y' );
+
 ?>
 
 <div class="content-section">
     <article class="post-wrapper">
-        <h2>Post title</h2>
+        <h2 class="post-wrapper__post-title">Post title</h2>
         <section class="post-wrapper__post-data">
             <ul>
                 <li>
-                    <i class="" aria-hidden="true"></i> 29/06/2016
+                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+                    <?php echo $post_date; ?>
                 </li>
                 <li>
                     <a href="#">
-                        <i class="" aria-hidden="true"></i>
+                        <i class="fa fa-sun-o" aria-hidden="true"></i>
                         Aula
                     </a>
                 </li>
                 <li>
-                    <i class="" aria-hidden="true"></i> 5
+                    <i class="fa fa-clock-o" aria-hidden="true"></i> 5
                 </li>
             </ul>
         </section>
@@ -42,7 +45,7 @@ get_header();
             </p>
             <img src="http://lorempixel.com/500/300" alt="">
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas posuere fringilla. Curabitur at ullamcorper metus. Quisque eleifend purus eu justo egestas, at feugiat velit fermentum. Etiam eget consectetur purus. Curabitur id ante porttitor, scelerisque nisi nec, bibendum nulla. Praesent faucibus nisl vel orci ultrices vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam elementum aliquet tincidunt. Integer ultrices posuere ipsum eu congue. Mauris vestibulum suscipit orci sit amet suscipit. Cras eu urna iaculis, pellentesque sapien vitae, mollis mi. Duis vel felis at turpis mattis posuere eu id velit. Nunc vitae pulvinar tellus.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas posuere fringilla. Curabitur at ullamcorper metus. Quisque eleifend purus eu justo egestas, at feugiat velit fermentum. Etiam eget consectetur purus. Curabitur id ante porttitor, scelerisque nisi nec, bibendum nulla. Praesent faucibus nisl vel orci ultrices vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam elementum aliquet tincidunt. Integer ultrices posuere ipsum eu congue. Mauris vestibulum suscipit orci sit amet suscipit. Cras eu urna iaculis, pellentesque sapien vitae, mollis mi. Duis vel felis at turpis mattis posuere eu id velit. Nunc vitae pulvinar tellus.
             </p>
         </section>
     </article>
@@ -55,16 +58,28 @@ get_header();
  // comments
  include( locate_template( 'comments.php' ) );
 
- // include sidebar.
- get_sidebar();
-
 ?>
 
 </div>
 
 <?php
 
+// include sidebar.
+get_sidebar();
+
+?>
+
+<div class="bottom-modules-wrapper">
+
+<?php
+
 // module 013.
-include( locate_template( 'modules/m013-newsletter.php' ) );
+include( locate_template( 'modules/m013-newsletter-section.php' ) );
+
+?>
+
+</div>
+
+<?php
 
 get_footer();
