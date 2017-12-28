dc563def0c0a34056840a45c41cbf9ca528981ce
davidguerreiro <me@davidguerreiro.com>
2017-12-28 01:19:50 +0100

    add m15 html code and add sass module

--- modules/m015-add-comment.php
+++ modules/m015-add-comment.php
@@ -7,3 +7,17 @@
  */
 
  ?>
+
+ <div class="module m15">
+    <h2 class="m15__title">
+        <i class="fa fa-comment-o icon" aria-hidden="true"></i>
+        Comenta algo
+    </h2>
+    <form action="" class="comment-form">
+        <input type="text" name="name" placeholder="Nombre" class="commet-form__input-text" required>
+        <textarea name="message" class="comment-form__textarea" required>
+            Message
+        </textarea>
+        <input type="submit" value="Comentar" class="commet-form__input-submit">
+    </form>
+ </div>
