d68205380180e5f452f0926c34f3597dae191645
davidguerreiro <me@davidguerreiro.com>
2017-12-31 20:27:01 +0100

    complete single post component

--- template-parts/post-single-item.php
+++ template-parts/post-single-item.php
@@ -13,26 +13,30 @@
 
  <article class="post-item">
      <a href="#" class="post-item__image-link">
-        <img src="http://lorempixel.com/500/300" alt="">
+        <img src="http://lorempixel.com/500/300" alt="" class="post-item__featured-image">
     </a>
-     <div class="post-item__data">
+     <div>
         <a href="#" class="post-item__title-link">
             <h3 class="post-item__data--title">
                 Adelante aunque solo tengas una camara.
             </h3>
         </a>
-        <ul>
-            <li>
+        <ul class="post-item__data">
+            <li class="post-item__data--element">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+                Angel
+            </li>
+            <li class="post-item__data--element">
                 <i class="fa fa-clock-o" aria-hidden="true"></i>
                 <?php echo $post_date; ?>
             </li>
-            <li>
-                <a href="post-item_category-link">
+            <li class="post-item__data--link-element">
+                <a href="#" class="post-item__category-link">
                     <i class="fa fa-sun-o" aria-hidden="true"></i>
                     Actividades
                 </a>
             </li>
-            <li>
+            <li class="post-item__data--element">
                 <i class="fa fa-comment-o" aria-hidden="true"></i>
                 5
             </li>
