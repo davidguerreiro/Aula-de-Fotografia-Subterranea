18459d8da13f851de87198df993b4fad34e9dd28
davidguerreiro <me@davidguerreiro.com>
2018-01-07 19:20:15 +0000

    complete team page and team single item tempalte html and css

--- template-parts/team-single-item.php
+++ template-parts/team-single-item.php
@@ -12,25 +12,25 @@
  ?>
 
  <article class="team-item">
+    <h3 class="team-item__title">David Guerreiro</h3>
     <div class="team-item__wrapper">
         <div class="team-item__section">
             <img src="<?php echo $url; ?>" alt="" class="team-item__image">
         </div>
-        <div class="team-item__section">
-            <h3 class="team-item__title">David</h3>
+        <div class="team-item__section team-item__section--data">
             <ul class="team-item__data">
                 <li>
                     <i class="fa fa-id-badge" aria-hidden="true"></i>
                     Desarrollador Web
                 </li>
                 <li>
-                    <i class="" aria-hidden="true"></i>
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>
                     London, UK
                 </li>
             </ul>
+            <p class="team-item__description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas posuere fringilla. Curabitur at ullamcorper metus. Quisque eleifend purus eu justo egestas, at feugiat velit fermentum. Etiam eget consectetur purus. Curabitur id ante porttitor, scelerisque nisi nec, bibendum nulla. Praesent faucibus nisl vel orci ultrices vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam elementum aliquet tincidunt. Integer ultrices posuere ipsum eu congue. Mauris vestibulum suscipit orci sit amet suscipit. Cras eu urna iaculis, pellentesque sapien vitae, mollis mi. Duis vel felis at turpis mattis posuere eu id velit. Nunc vitae pulvinar tellus.
+            </p>
         </div>
-        <p class="team-item__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas posuere fringilla. Curabitur at ullamcorper metus. Quisque eleifend purus eu justo egestas, at feugiat velit fermentum. Etiam eget consectetur purus. Curabitur id ante porttitor, scelerisque nisi nec, bibendum nulla. Praesent faucibus nisl vel orci ultrices vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam elementum aliquet tincidunt. Integer ultrices posuere ipsum eu congue. Mauris vestibulum suscipit orci sit amet suscipit. Cras eu urna iaculis, pellentesque sapien vitae, mollis mi. Duis vel felis at turpis mattis posuere eu id velit. Nunc vitae pulvinar tellus.
-        </p>
     </div>
  </article>
