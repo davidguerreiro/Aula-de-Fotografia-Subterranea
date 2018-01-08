006747601781d20cd1a986e8a72834f9fa421e59
davidguerreiro <me@davidguerreiro.com>
2018-01-08 22:57:24 +0000

    fix module 14 search sidebar style

--- modules/m014-sidebar-search-form.php
+++ modules/m014-sidebar-search-form.php
@@ -12,8 +12,9 @@
 
  <div class="module m14">
      <h2>Busqueda</h2>
+     <!--
      <form action="" method="get" class="m14__form">
-         <input type="hidden" name="nonce" value="<?php echo $nonce; ?>">
+         <input type="hidden" name="nonce" value="<?php // echo $nonce; ?>">
          <div class="search-wrapper">
              <input type="search" name="s" required>
              <button type="submit" class="btn-sidebar-submit">
@@ -21,6 +22,13 @@
              </button>
          </div>
      </form>
+-->
+    <?php
+        // module 26.
+        $is_sidebar = true;
+        include( locate_template( 'modules/m026-search-page-form.php' ) );
+        unset( $is_sidebar );
+    ?>
      <p>
          No te hace falta entrar en una cueva para encontrar algo en esta
          pagina, al menos por ahora.
--- modules/m026-search-page-form.php
+++ modules/m026-search-page-form.php
@@ -6,9 +6,10 @@
  * @package aula/modules
  */
 
- $s_term        = ( isset( $_GET['s'] ) ) ? sanitize_text_field( $_GET['s'] ) : '';
- $nonce         = wp_create_nonce( 'search_page_form' );
- $form_class    = ( is_404() ) ? "search-page-form search-page-form__404" : "search-page-form";
+ $s_term            = ( isset( $_GET['s'] ) ) ? sanitize_text_field( $_GET['s'] ) : '';
+ $nonce             = wp_create_nonce( 'search_page_form' );
+ $form_class        = ( is_404() ) ? "search-page-form search-page-form__404" : "search-page-form";
+ $text_input_class  = ( isset( $is_sidebar ) && $is_sidebar ) ? "search-page-form__input-text search-page-form__input-text--sidebar" : "search-page-form__input-text";
 
  ?>
 
@@ -16,7 +17,7 @@
     <form action="" method="get" class="<?php echo $form_class; ?>">
         <input type="hidden" name="nonce" value="<?php echo $nonce; ?>">
         <input type="search" name="s" value="<?php echo $s_term; ?>"
-        class="search-page-form__input-text" 
+        class="<?php echo $text_input_class; ?>" 
         placeholder="Busqueda ... " 
         required> 
         <button type="submit" class="btn-search-page-submit">
