1c81a27e2aae955f46f11aa43d761c9e021f4936
davidguerreiro <me@davidguerreiro.com>
2018-01-24 23:08:31 +0000

    complete module 06 php code

--- modules/m006-load-more.php
+++ modules/m006-load-more.php
@@ -6,13 +6,15 @@
  * @package aula/modules
  */
 
+ $blog_page_id = get_option( 'page_for_posts' );
+
  ?>
 
  <div class="module m6">
     <div class="m6__section m6__section-text">
         <p>Ver mas entradas</p>
     </div>
-    <a href="#" class="m6__section m6__section-link">        
+    <a href="<?php echo get_permalink( $blog_page_id ); ?>" class="m6__section m6__section-link">
         <i class="fa fa-arrow-right" aria-hidden="true"></i>
     </a>
  </div>
