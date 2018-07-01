f9ed4449b494e81cca938722c09dfc225bce0981
davidguerreiro <me@davidguerreiro.com>
2018-07-01 16:44:55 +0100

    fix typo error in pagination

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -44,7 +44,7 @@ if ( $total_pages > 1  ) {
 
         // display last link  - not done automatically by the function.
         if ( $current_page < $total_pages ) :
-            echo "<a href='" . esc_url( $last_page ) . "' class='next page-numbers last'>'ÚLTIMO</a>";
+            echo "<a href='" . esc_url( $last_page ) . "' class='next page-numbers last'>ÚLTIMO</a>";
         endif;
         echo '</nav>';
 }
