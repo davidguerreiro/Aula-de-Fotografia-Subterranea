5db7775e0cbf206eec654b231e7b40876a9fbf23
David <9396157+davidguerreiro@users.noreply.github.com>
2018-07-02 17:42:39 +0100

    add lazy loader JS to content images

--- js/scripts.js
+++ js/scripts.js
@@ -269,7 +269,33 @@ $(document).ready( function() {
         } else {
             $mobile_section.slideUp();
         }
-     });  
+     }); 
+
+    
+    // lazy loading images.
+    document.addEventListener("DOMContentLoaded", function() {
+        var lazyImages = [].slice.call(document.querySelectorAll("div.content-section img"));
+        
+        if ("IntersectionObserver" in window) {
+            let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+              entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                  let lazyImage = entry.target;
+                  lazyImage.src = lazyImage.dataset.src;
+                  lazyImage.srcset = lazyImage.dataset.srcset;
+                  lazyImage.classList.remove("lazy");
+                  lazyImageObserver.unobserve(lazyImage);
+                }
+              });
+            });
+        
+            lazyImages.forEach(function(lazyImage) {
+              lazyImageObserver.observe(lazyImage);
+            });
+          } else {
+            // Possibly fall back to a more compatible method here
+          }
+    });
 
   
 });
