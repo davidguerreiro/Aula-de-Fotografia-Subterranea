03a8948a56f6555d96335cce1b65e1c7b1096e50
David <9396157+davidguerreiro@users.noreply.github.com>
2018-07-23 17:56:45 +0100

    add module 31 instagram sidebar file code

---
+++ modules/m031-sidebar-instagram.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Module 031 - Sidebar - Facebook Group
+ * 
+ * This module is only used on the sidebar
+ * 
+ * @package aula/modules
+ */
+
+?>
+
+<div class="module m31">
+</div>
