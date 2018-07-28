57da611e3486f7219947408e9c7458517e046f6c
davidguerreiro <me@davidguerreiro.com>
2018-07-28 18:07:24 +0100

    add php code to instagram sidebar module

--- modules/m032-sidebar-instagram-group.php
+++ modules/m032-sidebar-instagram-group.php
@@ -8,13 +8,22 @@
  * @package aula/modules
  */
 
- ?>
+if ( isset( $args ) ) :
+ $key           = 'widget_' . $args['widget_id'];
+ $text          = get_field( 'sidebar_instagram_text', $key );
+ $external_link = get_field( 'sidebar_instagram_link', $key );
+
+ if ( ! empty( $external_link ) ) : ?>
 
 <div class="module m32">
-    <a href="#" class="m32__icon-link" target="_blank" title="Ir a Instagram">
+    <a href="<?php echo esc_url( $external_link ); ?>" class="m32__icon-link" target="_blank" title="Ir a Instagram">
         <i class="fa fa-instagram m32__main-icon" aria-hidden="true"></i>
-        <p class="m32__text">
-            Mas fotos disponibles en nuestra cuenta de Instagram
-        </p>
+        <?php if ( ! empty( $text ) ) : ?>
+            <p class="m32__text">
+                <?php echo esc_html( $text ); ?>
+            </p>
+        <?php endif; ?>
     </a>
 </div>
+<?php endif;
+endif; ?>
